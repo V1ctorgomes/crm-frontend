e40e0a56ee9317a1e0be84bb0ab0d1a9d025e9e6
V1ctorgomes <paulovictor.gomes19@icloud.com>
2026-04-12 21:44:53 -0300

    teste para saber qual NEXT PUBLIC ESTA SENDO USADO

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ export default function Home() {
   const [apiMessage, setApiMessage] = useState('Carregando...');
   const [healthMessage, setHealthMessage] = useState('Carregando...');
 
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'SEM_VARIAVEL';
+
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/`)
+    fetch(`${apiUrl}/`)
       .then((res) => res.json())
       .then((data) => {
         setApiMessage(data.message);
@@ -16,7 +18,7 @@ export default function Home() {
         setApiMessage('Falha ao consultar backend');
       });
 
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/health`)
+    fetch(`${apiUrl}/health`)
       .then((res) => res.json())
       .then((data) => {
         setHealthMessage(`${data.status} - ${data.service}`);
@@ -24,11 +26,12 @@ export default function Home() {
       .catch(() => {
         setHealthMessage('Falha ao consultar /health');
       });
-  }, []);
+  }, [apiUrl]);
 
   return (
     <main style={{ padding: '40px', fontFamily: 'Arial' }}>
       <h1>Frontend funcionando!</h1>
+      <p>API URL usada: {apiUrl}</p>
       <p>Mensagem do backend: {apiMessage}</p>
       <p>Status da API: {healthMessage}</p>
     </main>
